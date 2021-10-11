83a7d9bdea50b7578a3f12fa79cc1c12508a1578
mwz73trz <michaelzura@bellsouth.net>
2021-10-11 12:49:24 -0400

    auth gtg

--- contact_proj/asgi.py
+++ contact_proj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for contacts_proj project.
+ASGI config for contact_proj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contacts_proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contact_proj.settings')
 
 application = get_asgi_application()
--- contact_proj/settings.py
+++ contact_proj/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y4h9=-p9e%3mue(0!7yh$4w327^e=jt_k$s(fhby-gf&dheq^8'
+SECRET_KEY = 'django-insecure-2vcxh!h=t0l%^k26*5(&jsaz#!r6a^tz9t!ru9wlt6snn=mfn9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'contacts_proj.urls'
+ROOT_URLCONF = 'contact_proj.urls'
 
 TEMPLATES = [
     {
@@ -56,7 +56,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'contacts_proj.wsgi.application'
+WSGI_APPLICATION = 'contact_proj.wsgi.application'
 
 
 # Database
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'contacts_proj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'contacts',
+        'NAME': 'contacts_db',
     }
 }
 
